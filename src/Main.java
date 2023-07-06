e5d0f343349fe3b9cdbc18c74521d0c23cea68dc
emre099 <138773739+emre099@users.noreply.github.com>
2023-07-06 17:02:02 +0300

    Брой опити

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,18 @@
+import java.util.Scanner;
 public class Main {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
         System.out.println("Madhouse");
+        String word = "Програмиране";
+        int maxGuesses = 7;
+        int wrongGuesses = 0;
+        StringBuilder guessedLetters = new StringBuilder();
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Нека да започнем с играта Бесеница!");
+        System.out.println("Трябва да познаите думата, като въвеждате по една буква всеки път.");
+        System.out.println("Можете да кажете само " + maxGuesses + " грешни букви, преди да загубите.");
+
     }
 }
